c41682dad8d0f7040a67323436c974f0500704e9
julien <julien.thomas84@gmail.com>
2022-06-01 01:11:17 +0200

    feat(subgraph): index morpho borrowers

---
+++ subgraph/src/mapping.ts
@@ -0,0 +1,22 @@
+import { Address } from "@graphprotocol/graph-ts";
+import { Borrowed, Supplied } from "../generated/Morpho/Morpho";
+import { User } from "../generated/schema";
+
+function addUser(address: Address, isBorrower: boolean): void {
+  let user = User.load(address.toHex());
+
+  if (!user) {
+    user = new User(address.toHex());
+    user.address = address;
+    user.isBorrower = false;
+  }
+  user.isBorrower = user.isBorrower || isBorrower;
+  user.save();
+}
+
+export function handleBorrowed(event: Borrowed): void {
+  addUser(event.params._borrower, true);
+}
+export function handleSupplied(event: Supplied): void {
+  addUser(event.params._onBehalf, true);
+}
