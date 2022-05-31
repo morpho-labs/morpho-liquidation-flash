c2ff7de4da3eedd724b344ba37f66b77c5710410
julien <julien.thomas84@gmail.com>
2022-05-31 15:19:36 +0200

    feat(setup): add dependencies

---
+++ contracts/interface/IVatDaiFlashBorrower.sol
@@ -0,0 +1,21 @@
+
+pragma solidity 0.8.13;
+
+interface IVatDaiFlashBorrower {
+
+    /**
+     * @dev Receive a flash loan.
+     * @param initiator The initiator of the loan.
+     * @param amount The amount of tokens lent. [rad]
+     * @param fee The additional amount of tokens to repay. [rad]
+     * @param data Arbitrary data structure, intended to contain user-defined parameters.
+     * @return The keccak256 hash of "IVatDaiFlashLoanReceiver.onVatDaiFlashLoan"
+     */
+    function onVatDaiFlashLoan(
+        address initiator,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata data
+    ) external returns (bytes32);
+
+}
